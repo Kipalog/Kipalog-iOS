eb4233c87b29508f8daefa80270b1a4ba48a1d21
Orakaro <nhatminh_179@hotmail.com>
2017-12-21 21:22:02 +0900

    Initialize

---
+++ Kipalog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kipalog
+//
+//  Created by DTVD on 2017/12/21.
+//  Copyright © 2017 Kipalog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
