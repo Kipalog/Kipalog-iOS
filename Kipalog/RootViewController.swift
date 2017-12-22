699060c2876eff41d3409ac95ebf2b5910fc1eff
Orakaro <nhatminh_179@hotmail.com>
2017-12-22 16:46:31 +0900

    Restructure

--- Kipalog/RootViewController.swift
+++ Kipalog/RootViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
