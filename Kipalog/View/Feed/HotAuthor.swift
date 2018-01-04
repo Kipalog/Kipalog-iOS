8f145cfaa5c80aa4efd8000bbb8b2fe2e04b17d3
Orakaro <nhatminh_179@hotmail.com>
2018-01-05 00:03:57 +0900

    Add HotAuthor View

---
+++ Kipalog/View/Feed/HotAuthor.swift
@@ -0,0 +1,33 @@
+//
+//  HotAuthor.swift
+//  Kipalog
+//
+//  Created by DTVD on 2018/01/04.
+//  Copyright © 2018 Kipalog. All rights reserved.
+//
+
+import UIKit
+
+class HotAuthor: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var collectionVIew: UICollectionView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        Bundle.main.loadNibNamed("HotAuthor", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+}
